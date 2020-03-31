edafc96e024669c54163bafa88da94efa419f6aa
kennyzx <ikennyzx@gmail.com>
2020-03-31 15:30:51 +0800

    Add entity class 'Product'

---
+++ PlaySprintBoot/src/main/java/com/kennyzx/demo/models/Product.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kennyzx.demo.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author kennyzx
+ */
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String prodName;
+
+    private String prodDesc;
+
+    private String prodImage;
+
+    private Double prodPrice;
+
+    public Product() {
+    }
+
+    public Product(String prodName, String prodDesc, String prodImage, Double prodPrice) {
+        this.prodName = prodName;
+        this.prodDesc = prodDesc;
+        this.prodImage = prodImage;
+        this.prodPrice = prodPrice;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProdName() {
+        return prodName;
+    }
+
+    public void setProdName(String prodName) {
+        this.prodName = prodName;
+    }
+
+    public String getProdDesc() {
+        return prodDesc;
+    }
+
+    public void setProdDesc(String prodDesc) {
+        this.prodDesc = prodDesc;
+    }
+
+    public String getProdImage() {
+        return prodImage;
+    }
+
+    public void setProdImage(String prodImage) {
+        this.prodImage = prodImage;
+    }
+
+    public Double getProdPrice() {
+        return prodPrice;
+    }
+
+    public void setProdPrice(Double prodPrice) {
+        this.prodPrice = prodPrice;
+    }
+    
+}
